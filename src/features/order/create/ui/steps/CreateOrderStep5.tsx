495ca41c483247bff6df6a2a4c160b115981acad
Владимир <qtter85@gmail.com>
2024-07-03 00:55:47 +0300

    Init

--- src/features/order/create/ui/steps/CreateOrderStep5.tsx
+++ src/features/order/create/ui/steps/CreateOrderStep5.tsx
@@ -1,13 +1,41 @@
 'use client';
 
 import {CreateOrderStepTemplate} from "@/features/order/create/ui/steps/CreateOrderStepTemplate";
-
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
 
 export const CreateOrderStep5 = () => {
 
     return (
         <CreateOrderStepTemplate title='Куда и откуда?' description='Условия для каждого варианта различаются'>
+            <div className='flex flex-col mt-6 text-left w-screen px-12 sm:px-[25%] md:px-[30%] lg:px-[35%] xl:px-[35%]'>
+                <h1 className='text-2xl font-semibold'>На какой склад?</h1>
+                <Select>
+                    <SelectTrigger className="w-full mt-4">
+                        <SelectValue placeholder="Select a fruit" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+                            <SelectLabel>Склады Москвы</SelectLabel>
+                            <SelectItem value="apple">Вариант 1</SelectItem>
+                            <SelectItem value="banana">Вариант 2</SelectItem>
+                            <SelectItem value="blueberry">Вариант 3</SelectItem>
+                            <SelectItem value="grapes">Вариант 4</SelectItem>
+                            <SelectItem value="pineapple">Вариант 5</SelectItem>
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
 
+                <h1 className='text-2xl font-semibold mt-12'>Откуда забрать?</h1>
+                <input className='bg-[#2A2A2A] border-2 border-transparent mt-4 p-3 rounded-xl outline-none focus:border-[#666] placeholder-[#888]' placeholder='Московская 12...'/>
+            </div>
         </CreateOrderStepTemplate>
     )
 }
