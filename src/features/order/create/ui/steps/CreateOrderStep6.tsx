ba32c0a313f032ab8dc03f393c99b09b69a3b8e2
Владимир <qtter85@gmail.com>
2024-07-03 14:58:17 +0300

    Init

--- src/features/order/create/ui/steps/CreateOrderStep6.tsx
+++ src/features/order/create/ui/steps/CreateOrderStep6.tsx
@@ -6,8 +6,12 @@ import {CreateOrderStepTemplate} from "@/features/order/create/ui/steps/CreateOr
 export const CreateOrderStep6 = () => {
 
     return (
-        <CreateOrderStepTemplate title='Что доставить?' description='Условия для каждого варианта различаются'>
-
+        <CreateOrderStepTemplate title='Когда выполнить?' description='Условия для каждого варианта различаются'>
+            <div
+                className='flex flex-col mt-6 text-left w-screen px-12 sm:px-[25%] md:px-[30%] lg:px-[35%] xl:px-[35%]'>
+                <h1 className='text-2xl font-semibold'>Забрать</h1>
+                <h1 className='text-2xl font-semibold'>Доставить</h1>
+            </div>
         </CreateOrderStepTemplate>
-    )
+)
 }
