b16135b522cee39958ae4f173f313780f024ce8e
Владимир <qtter85@gmail.com>
2024-07-08 22:13:26 +0300

    Init

--- src/features/order/create/ui/steps/CreateOrderStep5SecondVariant.tsx
+++ src/features/order/create/ui/steps/CreateOrderStep5SecondVariant.tsx
@@ -42,7 +42,7 @@ export const CreateOrderStep5SecondVariant = () => {
     return (
         <CreateOrderStepTemplate title='Куда и откуда?' description='Условия для каждого варианта различаются'>
             <div
-                className='flex flex-col mt-6 text-left w-screen px-12 sm:px-[25%] md:px-[30%] lg:px-[35%] xl:px-[35%]'>
+                className='flex flex-col mt-2 text-left w-screen px-12 sm:px-[25%] md:px-[30%] lg:px-[35%] xl:px-[35%]'>
                 <h1 className='text-2xl font-semibold'>Куда доставить?</h1>
                 <div className='w-full max-h-36 overflow-y-auto pr-4 mt-4 pb-4'>
                     {inputs.map(i => (
